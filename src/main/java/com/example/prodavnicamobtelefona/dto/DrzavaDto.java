4a0e91f1395d3d7b44729ce91aedbd9dae3b58c3
MarkoProg01 <mmirkov2706@gmail.com>
2022-08-03 23:43:34 +0200

    Finish

--- src/main/java/com/example/prodavnicamobtelefona/dto/DrzavaDto.java
+++ src/main/java/com/example/prodavnicamobtelefona/dto/DrzavaDto.java
@@ -1,6 +1,8 @@
 package com.example.prodavnicamobtelefona.dto;
 
 import com.example.prodavnicamobtelefona.entity.Drzava;
+import com.example.prodavnicamobtelefona.entity.Kupac;
+import com.example.prodavnicamobtelefona.entity.Mobilni;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,6 +11,7 @@ import lombok.Data;
 public class DrzavaDto {
     private int id;
     private String nazivDrzave;
+
 
     public DrzavaDto(Drzava drzava) {
         this.id = drzava.getId();
--- src/main/java/com/example/prodavnicamobtelefona/dto/RacunDto.java
+++ src/main/java/com/example/prodavnicamobtelefona/dto/RacunDto.java
@@ -1,5 +1,7 @@
 package com.example.prodavnicamobtelefona.dto;
 
+import com.example.prodavnicamobtelefona.entity.Kupac;
+import com.example.prodavnicamobtelefona.entity.Mobilni;
 import com.example.prodavnicamobtelefona.entity.Racun;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,10 +12,14 @@ public class RacunDto {
     private int id;
     private int ukupna_cena;
     private int kolicina;
+    private Mobilni mobilni;
+    private Kupac kupac;
 
     public RacunDto(Racun racun) {
         this.id = racun.getId();
         this.ukupna_cena = racun.getUkupna_cena();
         this.kolicina = racun.getKolicina();
+        this.mobilni = racun.getMobilni();
+        this.kupac = racun.getKupac();
     }
 }
